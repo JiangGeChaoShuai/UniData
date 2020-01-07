65236155594358fd61c39920b47eee4c1ad1e057
EVA_CHEN <EVA_CHEN@10.240.157.96>
2020-01-07 12:33:31 -0500

    01/07

---
+++ src/Displays/StudentCenterState.java
@@ -0,0 +1,25 @@
+package Displays;
+
+import java.awt.event.ActionEvent;
+
+public class StudentCenterState extends State {
+	
+	//
+	
+	
+	
+	@Override
+	public void init() {
+		
+	}
+
+	@Override
+	public void addJComponents() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		
+	}
+}
