26c4aac355deb4a47bb12125c29a23d20f314183
JiangGeChaoShuai <57148706+JiangGeChaoShuai@users.noreply.github.com>
2020-01-10 19:17:10 -0500

    Update LoginFrame.java

--- src/Displays/LoginFrame.java
+++ src/Displays/LoginFrame.java
@@ -1,4 +1,7 @@
 package Displays;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -62,27 +65,40 @@ public class LoginFrame implements ActionListener {
 
 	private void createLayout() {
 
+		
+		Color TextColor = new Color(54,73,88);
+		Font font = new Font("Aileron", Font.PLAIN, 30);
+		
 		Frame.setLayout(null);
 
 		inputPanel.setBounds(0,0,width, height);
 		inputPanel.setLayout(null);
+		inputPanel.setBackground(new Color(201, 228, 202));
 		
 		Frame.add(inputPanel);
 		
 		JLabel title = new JLabel("Login Frame", SwingConstants.CENTER);
 		title.setBounds(Frame.getWidth() / 2 - 150, 50, 300, 50);
 		title.setVisible(true);
+		title.setForeground(TextColor);
+		title.setFont(font);
+		
 		inputPanel.add(title);
 
 		TeacherOrStudents.setBounds(Frame.getWidth() / 2 - 150, 100, 300, 25);
 		TeacherOrStudents.addItem("Teacher");
 		TeacherOrStudents.addItem("Student");
+		
 
 		inputPanel.add(TeacherOrStudents);
 
 		JLabel username = new JLabel("username", SwingConstants.CENTER);
 		username.setBounds(Frame.getWidth() / 2 - 150, 175, 300, 50);
 		username.setVisible(true);
+		username.setFont(font);
+
+		username.setForeground(TextColor);
+		
 		inputPanel.add(username);
 
 		usernameEntry.setBounds(Frame.getWidth() / 2 - 150, 225, 300, 50);
@@ -92,20 +108,28 @@ public class LoginFrame implements ActionListener {
 		JLabel password = new JLabel("password", SwingConstants.CENTER);
 		password.setBounds(Frame.getWidth() / 2 - 150, 325, 300, 50);
 		password.setVisible(true);
+		password.setForeground(TextColor);
+		password.setFont(font);
 		inputPanel.add(password);
 
 		passwordEntry.setBounds(Frame.getWidth() / 2 - 150, 375, 300, 50);
 		passwordEntry.setVisible(true);
 		inputPanel.add(passwordEntry);
 
-		login.setBounds(Frame.getWidth() / 2 - 150, 500, 100, 50);
+		login.setBounds(Frame.getWidth() / 2 - 150, 475, 300, 50);
 		login.setVisible(true);
 		login.addActionListener(this);
+		login.setForeground(new Color(255,255,255));
+		login.setBackground(TextColor);
+		login.setFont(font);
 		inputPanel.add(login);
 
-		signup.setBounds(Frame.getWidth() / 2 + 50, 500, 100, 50);
+		signup.setBounds(Frame.getWidth() / 2 - 150, 535, 300, 50);
 		signup.setVisible(true);
 		signup.addActionListener(this);
+		signup.setBackground(TextColor);
+		signup.setForeground(new Color(255,255,255));
+		signup.setFont(font);
 		inputPanel.add(signup);
 
 	}
@@ -119,8 +143,12 @@ public class LoginFrame implements ActionListener {
 		if (e.getSource() == signup) {
 
 			//inputPanel.setVisible(false);
-			SaveFile.SaveInformation();
+			SignUpFrame signUp = new SignUpFrame();
+			signUp.getPanel().setVisible(true);
+			Frame.setLayout(new BorderLayout());
+			Frame.getContentPane().add(signUp.getPanel(), BorderLayout.CENTER);
 			
+			inputPanel.setVisible(false);
 		}
 	}
 
